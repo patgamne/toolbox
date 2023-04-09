3d9d9b27415b10bdd5551ca44b89a09a5f0ff523
patgamne <118949633+patgamne@users.noreply.github.com>
2023-04-09 17:26:02 -0400

    Add files via upload

--- toolbox/scripts/bash/nmapscan/scan/nmap.sh
+++ toolbox/scripts/bash/nmapscan/scan/nmap.sh
@@ -4,9 +4,9 @@
 read -p "Enter IP address to scan: " ip_address
 
 # Run Nmap scan on entered IP address
-nmap -sV -O "$ip_address" -o /home/kali/gitclone/toolbox/results/nmap/nmap.xml
+nmap -sV -O "$ip_address" -o /home/kali/gitclone/toolbox/toolbox/results/nmap/nmap.xml
 
 # Display scan results
 echo "Scan complete. Results:"
-cat  /home/kali/gitclone/toolbox/results/nmap/nmap.xml
+cat  /home/kali/gitclone/toolbox/toolbox/results/nmap/nmap.xml
 
